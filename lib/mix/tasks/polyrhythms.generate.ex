4687f25b7c0324dbd6898719705379138a159fe2
Michael Berkowitz <michael.berkowitz@gmail.com>
2017-08-02 10:17:04 -0400

    Remove unused mix task and ignore saved scores

--- lib/mix/tasks/polyrhythms.generate.ex
+++
@@ -1,9 +0,0 @@
-defmodule Mix.Tasks.Polyrhythms.Generate do
-  use Mix.Task
-
-  @shortdoc "generate polyrhythms using given generator"
-  def run([version|_]) do
-    module = Module.safe_concat(PolyrhythmGenerator, version)
-    IO.inspect apply(module, :generate_parts, ["a"])
-  end
-end
