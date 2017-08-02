a6b9569e5869946bd94ef9ca7d78f134e401f314
Michael Berkowitz <michael.berkowitz@gmail.com>
2017-08-02 09:30:39 -0400

    refactor Measure.events_to_lily/1 and some module attribute cleanup

--- lib/measure.ex
+++ lib/measure.ex
@@ -6,7 +6,7 @@ defmodule Measure do
 
   def density(%__MODULE__{tuplet: nil}), do: 1.0
   def density(%__MODULE__{tuplet: {0, _}}), do: 0.0
-  def density(%__MODULE__{tuplet: {n, d}, events: events}) do
+  def density(%__MODULE__{tuplet: {n, _}, events: events}) do
     Enum.count(events, fn e -> e == "c8" end) / n
   end
 
@@ -23,19 +23,23 @@ defmodule Measure do
   def events_to_lily(%Measure{events: events, phoneme: phoneme, dynamic: nil}) do
     with [h|t] <- events do
       [h <> "^\\markup \"[#{phoneme}]\""| t]
-      |> List.insert_at(1, "[") |> List.insert_at(-1, "]") |> Enum.join(" ")
+      |> add_beaming() |> Enum.join(" ")
     end
   end
   def events_to_lily(%Measure{events: events, phoneme: nil, dynamic: dynamic}) do
     with [h|t] <- events do
       [h <> dynamic | t]
-      |> List.insert_at(1, "[") |> List.insert_at(-1, "]") |> Enum.join(" ")
+      |> add_beaming() |> Enum.join(" ")
     end
   end
   def events_to_lily(%Measure{events: events, phoneme: phoneme, dynamic: dynamic}) do
     with [h|t] <- events do
       [h <> dynamic <> "^\\markup \"[#{phoneme}]\"" | t]
-      |> List.insert_at(1, "[") |> List.insert_at(-1, "]") |> Enum.join(" ")
+      |> add_beaming() |> Enum.join(" ")
     end
+  end
+
+  def add_beaming(events) do
+    events |> List.insert_at(1, "[") |> List.insert_at(-1, "]")
   end
 end
